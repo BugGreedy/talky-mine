b975c58906e98eaaeb2004e566011b6832c3854f
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-09-29 10:23:02 +0900

    audioでも投稿できた(再生はまだ)又、トップページにuserイメージで投稿できた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :sounds
   has_many :comments
+  has_one_attached :image
 
   with_options presence: true do
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
@@ -10,5 +11,6 @@ class User < ApplicationRecord
     validates :nickname, uniqueness: true
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+    validates :image
   end
 end
