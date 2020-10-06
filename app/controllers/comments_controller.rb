de7dd33cd9ba8d6ec9ed52612a0a11e4b3327b44
BugGreedy <desire.cutton.bug.1130@gmail.com>
2020-10-06 15:17:40 +0900

    ブランチ切る前のcommit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,11 @@ class CommentsController < ApplicationController
   end
 
   def index
-    @comments = Comment.all(:order => "created_at DESC")
+    @comments = Comment.all(order: 'created_at DESC')
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, sound_id: params[:sound_id])
   end
